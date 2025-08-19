9e8a70f9391fcc2db1f8ded75d2e4986183fa8d2
deadlauncherg <113706114+deadlauncherg@users.noreply.github.com>
2025-08-19 15:33:56 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,13 @@
-index.js 
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  if (!req.session.user) return res.redirect("/login");
+  res.render("dashboard", { user: req.session.user });
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy(() => res.redirect("/login"));
+});
+
+module.exports = router;
